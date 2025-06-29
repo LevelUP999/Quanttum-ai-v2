917264193f3317881dd9babfb805e54d87765242
LevelUP999 <levelup999.twitch.tv@gmail.com>
2025-06-29 20:19:54 -0300

    Add desmark to StudyActivity v5 (finaly?) opv1-v

--- src/pages/StudyActivity.tsx
+++ src/pages/StudyActivity.tsx
@@ -388,7 +388,7 @@ const StudyActivity = () => {
                   </p>
                   <Button
                     onClick={desmark}
-                    className="w-full bg-gradient-to-r from-green-600 to-emerald-600 hover:from-green-700 hover:to-emerald-700"
+                    className="w-full bg-gradient-to-r from-indigo-600 to-violet-600 hover:from-green-700 hover:to-emerald-700"
                   >
                     <CheckCircle className="w-4 h-4 mr-2" />
                     Marcar como Não Concluída
